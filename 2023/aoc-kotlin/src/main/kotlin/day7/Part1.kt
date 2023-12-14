eeae1f79ce4ecb7d08b3c27229e44524a0453cc1
huib <1674545+huib@users.noreply.github.com>
2023-12-14 15:48:38 +0100

    day 7

--- 2023/aoc-kotlin/src/main/kotlin/day7/Part1.kt
+++ 2023/aoc-kotlin/src/main/kotlin/day7/Part1.kt
@@ -10,7 +10,28 @@ fun main() {
 fun day7p1(input: String): String {
     val lines = input.split("\n").map { it.trim() }
 
-    val returnValue = lines.size
+    val cards = lines.map(::Hand).sortedWith { hand1, hand2 ->
+        (hand1.grouped + hand1.cardValues)
+            .zip(hand2.grouped + hand2.cardValues)
+            .firstOrNull { (a, b) -> a != b }
+            ?.let { (a, b) -> compareValues(a, b) }
+            ?: 0
+    }
+    val returnValue = cards.mapIndexed { index, hand -> hand.score * (index + 1) }.sum()
 
     return returnValue.toString()
+}
+
+class Hand(str: String) {
+    val score = str.split(" ").last().toInt()
+    val cardValues = str.split(" ").first().map { value[it]!! }
+    val grouped = cardValues.groupBy { it }.map { it.value.size }.sorted().reversed()
+
+    override fun toString(): String {
+        return "Card: $cardValues, $grouped, $score"
+    }
+
+    companion object {
+        val value = (('2'..'9') + 'T' + 'J' + 'Q' + 'K' + 'A').mapIndexed { index: Int, c: Char -> c to index }.toMap()
+    }
 }
--- 2023/aoc-kotlin/src/main/kotlin/day7/Part2.kt
+++ 2023/aoc-kotlin/src/main/kotlin/day7/Part2.kt
@@ -10,7 +10,31 @@ fun main() {
 fun day7p2(input: String): String {
     val lines = input.split("\n").map { it.trim() }
 
-    val returnValue = lines.size
+    val cards = lines.map(::Hand2).sortedWith { hand1, hand2 ->
+        (hand1.grouped + hand1.cardValues)
+            .zip(hand2.grouped + hand2.cardValues)
+            .firstOrNull { (a, b) -> a != b }
+            ?.let { (a, b) -> compareValues(a, b) }
+            ?: 0
+    }.onEach(::println)
+    val returnValue = cards.mapIndexed { index, hand -> hand.score * (index + 1) }.sum()
 
     return returnValue.toString()
+}
+
+class Hand2(val str: String) {
+    val score = str.split(" ").last().toInt()
+    val cardValues = str.split(" ").first().map { value[it]!! }
+    val numJokers = cardValues.count { it == value['J'] }
+    val grouped = cardValues.filterNot { it == value['J'] }.groupBy { it }.map { it.value.size }.sorted().reversed().mapIndexed { index, i ->
+        if (index == 0) i + numJokers else i
+    }.ifEmpty { listOf(5) }
+
+    override fun toString(): String {
+        return "Card $str: $grouped, $cardValues, $score"
+    }
+
+    companion object {
+        val value = (listOf('J') + ('2'..'9') + 'T' + 'Q' + 'K' + 'A').mapIndexed { index: Int, c: Char -> c to index }.toMap()
+    }
 }
