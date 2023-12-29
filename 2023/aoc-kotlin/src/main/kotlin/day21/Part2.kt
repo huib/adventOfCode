27ea51cc2fdd57fddfce4fa2df050ffb8df809bd
huib <1674545+huib@users.noreply.github.com>
2023-12-29 20:49:13 +0100

    day21 part2 refactor (still ideas for improvement)

--- 2023/aoc-kotlin/src/main/kotlin/day21/Part2.kt
+++ 2023/aoc-kotlin/src/main/kotlin/day21/Part2.kt
@@ -12,34 +12,43 @@ fun day21p2(input: String): String {
     val lines = input.split("\n").map { it.trim() }
     val field = InfiniteField(lines)
 
-    val numSteps = 6L
+    val numSteps = 26501365L // 26501365L
 
-    val centerCycle = findCycle(mapOf(field.startPosition to true), field::propagateCenter)
-    println(centerCycle.getState(numSteps).count { (pos, isPreceding) -> !isPreceding })
+    val centerCycle = findCycle(field.startState, field::propagateCenter)
+    centerCycle.printInfo(numSteps, "center")
 
-    val NECycle = findCycle(mapOf((0 to field.height - 1) to true), field::propagateCenter)
-    println(NECycle.getState(numSteps).count { (pos, isPreceding) -> !isPreceding })
-    val NWCycle = findCycle(mapOf((field.width - 1 to field.height - 1) to true), field::propagateCenter)
-    println(NWCycle.getState(numSteps).count { (pos, isPreceding) -> !isPreceding })
-    val SWCycle = findCycle(mapOf((field.width - 1 to 0) to true), field::propagateCenter)
-    println(SWCycle.getState(numSteps).count { (pos, isPreceding) -> !isPreceding })
-    val SECycle = findCycle(mapOf((0 to 0) to true), field::propagateCenter)
-    println(SECycle.getState(numSteps).count { (pos, isPreceding) -> !isPreceding })
-
-    val westCycle = findCycle(mapOf(field.startPosition to true), field::propagateLeft)
-    println(westCycle.getState(numSteps).count { (pos, isPreceding) -> !isPreceding })
-    val northCycle = findCycle(mapOf(field.startPosition to true), field::propagateUp)
-    println(northCycle.getState(numSteps).count { (pos, isPreceding) -> !isPreceding })
-    val eastCycle = findCycle(mapOf(field.startPosition to true), field::propagateRight)
-    println(eastCycle.getState(numSteps).count { (pos, isPreceding) -> !isPreceding })
-    val southCycle = findCycle(mapOf(field.startPosition to true), field::propagateDown)
-    println(southCycle.getState(numSteps).count { (pos, isPreceding) -> !isPreceding })
+//    val NECycle = findCycle(mapOf((0 to field.height - 1) to true), field::propagateCenter)
+//    println(NECycle.getState(numSteps).count { (pos, isPreceding) -> !isPreceding })
+//    val NWCycle = findCycle(mapOf((field.width - 1 to field.height - 1) to true), field::propagateCenter)
+//    println(NWCycle.getState(numSteps).count { (pos, isPreceding) -> !isPreceding })
+//    val SWCycle = findCycle(mapOf((field.width - 1 to 0) to true), field::propagateCenter)
+//    println(SWCycle.getState(numSteps).count { (pos, isPreceding) -> !isPreceding })
+//    val SECycle = findCycle(mapOf((0 to 0) to true), field::propagateCenter)
+//    println(SECycle.getState(numSteps).count { (pos, isPreceding) -> !isPreceding })
+//
+    val westCycle = findCycle(field.startState, field::propagateLeft)
+    westCycle.printInfo(numSteps, "west")
+    val northCycle = findCycle(field.startState, field::propagateUp)
+    northCycle.printInfo(numSteps, "north")
+    val eastCycle = findCycle(field.startState, field::propagateRight)
+    eastCycle.printInfo(numSteps, "east")
+    val southCycle = findCycle(field.startState, field::propagateDown)
+    southCycle.printInfo(numSteps, "south")
 
     val returnValue = 0
 
     return returnValue.toString()
 }
 
+fun CycleData<Triple<Pointset, Pointset, Pointset>>.printInfo(numSteps: Long, label: String) {
+    println("cycle $label: $cycleStart + $cycleLength * k")
+    println("   after $numSteps: ${getState(numSteps).let { it.first + it.third }.size}")
+}
+
+val InfiniteField.startState
+    get() = Triple<Pointset, Pointset, Pointset>(setOf(), setOf(), setOf(startPosition))
+
+typealias Pointset = Set<Pair<Int, Int>>
 class InfiniteField(val lines: List<String>) {
     val width = lines.first().length
     val height = lines.size
@@ -47,102 +56,120 @@ class InfiniteField(val lines: List<String>) {
     val startX = lines[startY].indexOf('S')
     val startPosition = startX to startY
 
-    val cache = mutableMapOf<Map<Pair<Int, Int>, Boolean>, Map<Pair<Int, Int>, Boolean>>()
-    fun propagate(previousPositions: Map<Pair<Int, Int>, Boolean>) = cache.getOrPut(previousPositions) {
-        return previousPositions.flatMap { (pos, isPrecedingStep) ->
-            if (isPrecedingStep) {
+    val cache = mutableMapOf<
+        Triple<Pointset, Pointset, Pointset>,
+        Triple<Pointset, Pointset, Pointset>,
+        >()
+
+    fun propagate(steps123: Triple<Pointset, Pointset, Pointset>) =
+        propagate(steps123.first, steps123.second, steps123.third)
+
+    fun propagate(step1: Pointset, step2: Pointset, step3: Pointset): Triple<Pointset, Pointset, Pointset> {
+        return cache.getOrPut(Triple(step1, step2, step3)) {
+            val step4 = step3.flatMapTo(mutableSetOf()) { pos ->
                 nbrs_not_diag(pos)
+                    .filterNot(step2::contains)
                     .filter(::isPassable)
-                    .filterNot(previousPositions::containsKey)
-                    .map { it to true } + (pos to false)
-            } else {
-                listOf(pos to true)
             }
-        }.toMap()
+            Triple(step2, step1 + step3, step4)
+        }
     }
 
-    fun propagateRight(previousPositions: Map<Pair<Int, Int>, Boolean>): Map<Pair<Int, Int>, Boolean> {
-        val newPositions = propagate(previousPositions)
-        val rightMostX = newPositions.filterValues { it }.keys.maxOf { (x, y) -> x }
+    fun propagateRight(steps123: Triple<Pointset, Pointset, Pointset>): Triple<Pointset, Pointset, Pointset> {
+        val (step2, step3, step4) = propagate(steps123)
+        val rightMostX = step4.maxOf { (x, y) -> x }
         val rightMostField = rightMostX / width
 
-        val shiftedPositions = if (rightMostField == 2) {
-            newPositions.mapKeys { (pos, _) ->
-                val (x, y) = pos
-                x - width to y
-            }
+        fun Pointset.shift() = if (rightMostField == 2) {
+            mapTo(mutableSetOf()) { (x, y) -> x - width to y }
         } else {
-            newPositions
+            this
         }
 
-        return shiftedPositions.filterKeys { (x, y) ->
+        fun Pointset.truncate() = filterTo(mutableSetOf()) { (x, y) ->
             y in 0..height && x in 0..(2 * width)
         }
+
+        return Triple(
+            step2.shift().truncate(),
+            step3.shift().truncate(),
+            step4.shift().truncate(),
+        )
     }
 
-    fun propagateLeft(previousPositions: Map<Pair<Int, Int>, Boolean>): Map<Pair<Int, Int>, Boolean> {
-        val newPositions = propagate(previousPositions)
-        val leftMostX = newPositions.filterValues { it }.keys.minOf { (x, y) -> x }
+    fun propagateLeft(steps123: Triple<Pointset, Pointset, Pointset>): Triple<Pointset, Pointset, Pointset> {
+        val (step2, step3, step4) = propagate(steps123)
+        val leftMostX = step4.minOf { (x, y) -> x }
         val leftMostField = (leftMostX - width) / width
 
-        val shiftedPositions = if (leftMostField == -2) {
-            newPositions.mapKeys { (pos, _) ->
-                val (x, y) = pos
-                x + width to y
-            }
+        fun Pointset.shift() = if (leftMostField == -2) {
+            mapTo(mutableSetOf()) { (x, y) -> x + width to y }
         } else {
-            newPositions
+            this
         }
 
-        return shiftedPositions.filterKeys { (x, y) ->
+        fun Pointset.truncate() = filterTo(mutableSetOf()) { (x, y) ->
             y in 0..height && x in (-1 * width)..width
         }
-    }
 
-    fun propagateUp(previousPositions: Map<Pair<Int, Int>, Boolean>): Map<Pair<Int, Int>, Boolean> {
-        val newPositions = propagate(previousPositions)
-        val upMostY = newPositions.filterValues { it }.keys.minOf { (x, y) -> y }
+        return Triple(
+            step2.shift().truncate(),
+            step3.shift().truncate(),
+            step4.shift().truncate(),
+        )
+    }
+    fun propagateUp(steps123: Triple<Pointset, Pointset, Pointset>): Triple<Pointset, Pointset, Pointset> {
+        val (step2, step3, step4) = propagate(steps123)
+        val upMostY = step4.minOf { (x, y) -> y }
         val upMostField = (upMostY - height) / height
 
-        val shiftedPositions = if (upMostField == -2) {
-            newPositions.mapKeys { (pos, _) ->
-                val (x, y) = pos
-                x to y + height
-            }
+        fun Pointset.shift() = if (upMostField == -2) {
+            mapTo(mutableSetOf()) { (x, y) -> x to y + height }
         } else {
-            newPositions
+            this
         }
 
-        return shiftedPositions.filterKeys { (x, y) ->
+        fun Pointset.truncate() = filterTo(mutableSetOf()) { (x, y) ->
             x in 0..width && y in (-1 * height)..height
         }
+
+        return Triple(
+            step2.shift().truncate(),
+            step3.shift().truncate(),
+            step4.shift().truncate(),
+        )
     }
 
-    fun propagateDown(previousPositions: Map<Pair<Int, Int>, Boolean>): Map<Pair<Int, Int>, Boolean> {
-        val newPositions = propagate(previousPositions)
-        val downMostY = newPositions.filterValues { it }.keys.maxOf { (x, y) -> y }
+    fun propagateDown(steps123: Triple<Pointset, Pointset, Pointset>): Triple<Pointset, Pointset, Pointset> {
+        val (step2, step3, step4) = propagate(steps123)
+        val downMostY = step4.maxOf { (x, y) -> y }
         val downMostField = downMostY / height
 
-        val shiftedPositions = if (downMostField == 2) {
-            newPositions.mapKeys { (pos, _) ->
-                val (x, y) = pos
-                x to y - height
-            }
+        fun Pointset.shift() = if (downMostField == 2) {
+            mapTo(mutableSetOf()) { (x, y) -> x to y - height }
         } else {
-            newPositions
+            this
         }
 
-        return shiftedPositions.filterKeys { (x, y) ->
+        fun Pointset.truncate() = filterTo(mutableSetOf()) { (x, y) ->
             x in 0..width && y in 0..(2 * height)
         }
+
+        return Triple(
+            step2.shift().truncate(),
+            step3.shift().truncate(),
+            step4.shift().truncate(),
+        )
     }
 
-    fun propagateCenter(previousPositions: Map<Pair<Int, Int>, Boolean>): Map<Pair<Int, Int>, Boolean> {
-        val newPositions = propagate(previousPositions)
+    fun propagateCenter(steps123: Triple<Pointset, Pointset, Pointset>): Triple<Pointset, Pointset, Pointset> {
+        val (step2, step3, step4) = propagate(steps123)
 
-        return newPositions.filterKeys { (x, y) ->
+        val step4InBounds = step4.filterTo(mutableSetOf()) { (x, y) ->
             x in 0..width && y in 0..height
         }
+
+        return Triple(step2, step3, step4InBounds)
     }
 
     fun isPassable(x: Int, y: Int) = lines[(y % height + height) % height][(x % width + width) % width] != '#'
@@ -151,23 +178,21 @@ class InfiniteField(val lines: List<String>) {
 
 fun <T> findCycle(start: T, transform: (T) -> T): CycleData<T> {
     val stateToIteration = mutableMapOf<T, Int>()
-    val iterationToResult = mutableMapOf<Int, T>()
+    val states = mutableListOf(start)
 
-    var state = start
-    var iteration = 0
     while (true) {
-        val repeatedIteration = stateToIteration.getOrPut(state) { iteration }
-        state = iterationToResult.getOrPut(repeatedIteration) { transform(state) }
+        val state = states.last()
+        val repeatedIteration = stateToIteration.getOrPut(state) { states.size - 1 }
 
-        if (repeatedIteration != iteration) {
+        if (repeatedIteration == states.size - 1) {
+            states.add(transform(state))
+        } else {
             return CycleData(
                 cycleStart = repeatedIteration,
-                cycleLength = iteration - repeatedIteration,
-                states = (0..<iteration).map(iterationToResult::getValue),
+                cycleLength = states.size - 1 - repeatedIteration,
+                states = states,
             )
         }
-
-        iteration++
     }
 }
 
