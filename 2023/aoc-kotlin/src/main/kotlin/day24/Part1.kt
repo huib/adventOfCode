bb54f8cdfd88bc9e4f5b6627853f81ae948f0fca
huib <1674545+huib@users.noreply.github.com>
2023-12-29 15:47:11 +0100

    day24 (using sage math for part 2...)

--- 2023/aoc-kotlin/src/main/kotlin/day24/Part1.kt
+++ 2023/aoc-kotlin/src/main/kotlin/day24/Part1.kt
@@ -1,6 +1,7 @@
 package day24
 
 import getInput
+import java.math.BigDecimal
 
 fun main() {
     val input = getInput("/input_day24")
@@ -8,9 +9,88 @@ fun main() {
 }
 
 fun day24p1(input: String): String {
-    val lines = input.split("\n").map { it.trim() }
+    val hailstones = input.split("\n").map(::parseHailstone)
+    val range = (200000000000000.toBigDecimal())..(400000000000000.toBigDecimal())
 
-    val returnValue = lines.size
+    var count = 0
+    hailstones.indices.forEach { index1 ->
+        (0..<index1).forEach { index2 ->
+            val stone1 = hailstones[index1]
+            val stone2 = hailstones[index2]
+
+            (stone1 intersection2d stone2)?.let { intersection ->
+                if (intersection.first in range && intersection.second in range) {
+                    count++
+                }
+            }
+        }
+    }
+    val returnValue = count
 
     return returnValue.toString()
 }
+
+data class Hailstone(
+    val x: BigDecimal,
+    val y: BigDecimal,
+    val z: BigDecimal,
+    val dx: BigDecimal,
+    val dy: BigDecimal,
+    val dz: BigDecimal,
+) {
+    init {
+        require(dx.compareTo(BigDecimal.ZERO) != 0)
+        require(dy.compareTo(BigDecimal.ZERO) != 0)
+        require(dz.compareTo(BigDecimal.ZERO) != 0)
+    }
+    infix fun intersects2d(other: Hailstone) = intersection2d(other) != null
+
+    infix fun intersection2d(other: Hailstone): Pair<BigDecimal, BigDecimal>? {
+        // (x0 + t*dx, y0 + t*dy) == (x0' + t'*dx', y0' + t'*dy')
+        // x = x0 + t*dx <=> t = (x - x0)/dx
+        // y = y0 * t*dy -> y = y0 + (x - x0) * dy/dx
+
+        // y == y`
+        // y0 + (x - x0) * dy/dx == y0' + (x - x0') * dy'/dx'
+        // y0 + x * dy/dx - x0 * dy/dx == y0' + x * dy'/dx' - x0' * dy'/dx'
+        // x * (dy/dx - dy'/dx') == y0' - y0 - x0' * dy'/dx' + x0 * dy/dx
+        // x == (y0' - y0 - x0' * dy'/dx' + x0 * dy/dx) / (dy/dx - dy'/dx')
+
+        // x == (y0' * dx * dx' - y0 * dx * dx' - x0' * dy' * dx + x0 * dy * dx') / (dy/dx - dy'/dx') / dx' / dx
+        // x == (y0' - y0) * (dx * dx' - x0' * dy' * dx + x0 * dy * dx') / ( dx * dx' * (dy * dx' - dy' * dx)/(dx' * dx) )
+        // x == (y0' - y0) * (dx^2 * dx'^2 - x0' * dy' * dx^2 * dx' + x0 * dy * dx'^2 * dx) / ( dx * dx' * (dy * dx' - dy' * dx) )
+
+        if (dx == 0.0.toBigDecimal() || other.dx == 0.0.toBigDecimal()) {
+            error("this doesn't happen in my input, so I don't try to deal with it :)")
+        }
+
+        if ((dx * other.dy).compareTo(other.dx * dy) == 0) {
+            // parallel, assume they don't intersect, otherwise I'm not sure if
+            // it counts as a single intersection or infinitely many.
+            return null
+        }
+
+        val px = (other.y - y - other.x * other.dy / other.dx + x * dy / dx) / (dy / dx - other.dy / other.dx)
+        val t1 = (px - x) / dx
+        val t2 = (px - other.x) / other.dx
+        val py = y + t1 * dy
+
+        if (t1.compareTo(BigDecimal.ZERO) == -1 || t2.compareTo(BigDecimal.ZERO) == -1) {
+            return null
+        }
+
+        return px to py
+    }
+}
+
+fun parseHailstone(str: String): Hailstone {
+    val (x, y, z, dx, dy, dz) = str
+        .split('@')
+        .flatMap { it.split(',') }
+        .map(String::trim)
+        .map { it.toBigDecimal().setScale(16) }
+
+    return (Hailstone(x, y, z, dx, dy, dz))
+}
+
+private operator fun <E> List<E>.component6() = get(5)
--- 2023/aoc-kotlin/src/main/kotlin/day24/Part2.kt
+++ 2023/aoc-kotlin/src/main/kotlin/day24/Part2.kt
@@ -8,9 +8,34 @@ fun main() {
 }
 
 fun day24p2(input: String): String {
-    val lines = input.split("\n").map { it.trim() }
+    val hailstones = input.split("\n").map(::parseHailstone)
 
-    val returnValue = lines.size
+    val sageMathScript = buildString {
+        appendLine("t1, t2, t0, sx, sy, sz, sdx, sdy, sdz = var('t1 t2 t0 sx sy sz sdx sdy sdz')")
+        appendLine("solve([")
+        hailstones.take(3).forEachIndexed { i, stone ->
+            with(stone) {
+                appendLine("    ${x.toBigInteger()} + ${dx.toBigInteger()} * t$i == sx + t$i * sdx,")
+                appendLine("    ${y.toBigInteger()} + ${dy.toBigInteger()} * t$i == sy + t$i * sdy,")
+                appendLine("    ${z.toBigInteger()} + ${dz.toBigInteger()} * t$i == sz + t$i * sdz,")
+            }
+        }
+        appendLine("], t1, t2, t0, sx, sy, sz, sdx, sdy, sdz)")
+    }
+    println(sageMathScript)
+
+    // sage math solution:
+    val t1 = 994990845610
+    val t2 = 792974158044
+    val t0 = 137307190534
+    val sx = 335849990884055
+    val sy = 362494628861890
+    val sz = 130073711567420
+    val sdx = -110
+    val sdy = -135
+    val sdz = 299
+
+    val returnValue = sx + sy + sz
 
     return returnValue.toString()
 }
